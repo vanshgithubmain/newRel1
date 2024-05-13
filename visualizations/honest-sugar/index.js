39659fc6673bb988a8357b3be2fe3c2af7decb32
vanshgithubmain <157695410+vanshgithubmain@users.noreply.github.com>
2024-05-13 15:16:31 +0530

    first commis2sss

--- visualizations/honest-sugar/index.js
+++ visualizations/honest-sugar/index.js
@@ -8,11 +8,13 @@ const index = () => {
   return (
     <div className="heading">
           <h1>hiii everyone</h1>
+         
         <AreaChart
         accountId={4438281}
             query="SELECT average(transmitBytesPerSecond) AS `Transmit bytes per second`, average(receiveBytesPerSecond) AS `Receive bytes per second` FROM NetworkSample WHERE (entityGuid = 'NDQzODI4MXxJTkZSQXxOQXw3MTQ0ODcwMDk3NTAyNDQ5ODQ1') TIMESERIES AUTO"
             fullWidth
             />
+          
             <LineChart
         accountId={4438281}
             query="SELECT average(transmitBytesPerSecond) AS `Transmit bytes per second`, average(receiveBytesPerSecond) AS `Receive bytes per second` FROM NetworkSample WHERE (entityGuid = 'NDQzODI4MXxJTkZSQXxOQXw3MTQ0ODcwMDk3NTAyNDQ5ODQ1') TIMESERIES AUTO"
@@ -23,9 +25,13 @@ const index = () => {
             query="SELECT average(transmitBytesPerSecond) AS `Transmit bytes per second`, average(receiveBytesPerSecond) AS `Receive bytes per second` FROM NetworkSample WHERE (entityGuid = 'NDQzODI4MXxJTkZSQXxOQXw3MTQ0ODcwMDk3NTAyNDQ5ODQ1') TIMESERIES AUTO"
             fullWidth
             />
+                <BarChart
+        accountId={4438281}
+            query="SELECT average(transmitBytesPerSecond) AS `Transmit bytes per second`, average(receiveBytesPerSecond) AS `Receive bytes per second` FROM NetworkSample WHERE (entityGuid = 'NDQzODI4MXxJTkZSQXxOQXw3MTQ0ODcwMDk3NTAyNDQ5ODQ1') TIMESERIES AUTO"
+            fullWidth
+            />
                
                
- 
     </div>
   );
 };
